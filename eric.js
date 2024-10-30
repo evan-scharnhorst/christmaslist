97d4a30fd6aa8949d425c58d1ca59bf80689e9ba
Evan S <58012114+evan-scharnhorst@users.noreply.github.com>
2024-10-30 10:51:58 -0400

    Update eric.js

--- eric.js
+++ eric.js
@@ -1,10 +1,10 @@
 const EricChristmasList = [
   {
-    image: 'https://via.placeholder.com/80',
+    image: 'https://m.media-amazon.com/images/I/71dgJV5uDhL._AC_SX679_.jpg',
     priority: 1,
-    price: 9.99,
-    link: 'https://example.com',
-    title: 'Example',
-    description: 'Example'
+    price: 25.99,
+    link: 'https://www.amazon.com/Skater-Trainers-Skateboard-Tricks-Skateboarding/dp/B01F49C8NI/ref=sr_1_2?crid=19XLV6Y5QWZYI&dib=eyJ2IjoiMSJ9.92SHqiY4cIR9GgJ-xhKPOEwZSpuHxsyhdv0t91pC-bWGS2lkcbUnLhl6Nup292JfofWy7SmZSm_w4Spg1jeV2tZFwpSw9zFCWbvOqr_Cdo1cNOQQV0e8Ys4lQQnIWhVhdjDVc2iP_YmC9RcM3NcTTlQeYoKyNw7z6AjRKxMSw-w3gCB7LkudMm69Hf3f8FiNzyD2fJZRQlvG3Lc0wJfIscxsJX8HXPRGEq4NJ47SYsUhVwvWh7AhMoIVAOHN_XCdnNpEubXmCnuFuZva9kigUbVKzzf_4GofWB2mW_Ud_WU.6Qmr6O8vK9NmHOCd5o5w3XB-jrAXycWrnmbPIOL1nLg&dib_tag=se&keywords=skateboard+wheel+stoppers&qid=1730299840&sprefix=skateboard+wheel+stopper%2Caps%2C197&sr=8-2',
+    title: 'Skateboard Wheel Covers',
+    description: 'It will help me improve my skateboard tricks while also risking less injury.'
   },
 ];
